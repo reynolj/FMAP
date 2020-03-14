08263cee8ebacc1316a3eb7d8099b16fbc6bdfed
reynolj <jakereynolds@csus.edu>
2020-03-13 19:48:01 -0700

    Did alot. Working on drawing function bubbles now

--- index.php
+++ index.php
@@ -6,10 +6,13 @@ require("structure/top.php"); //Include the sidebar HTML
 
 
 ?>
+
 <html>
   <head>
     <script src="mathjs/dist/math.js" type="text/javascript"></script>
-    <script src="d3/d3.js" type="text/javascript"></script>
+    <script src="d3/d3.js" type="text/javascript">
+        import * as d3 from "/d3/d3.js";
+    </script>
     <script type="text/javascript">
       let gDomain;
       let gCodomain;
@@ -21,19 +24,71 @@ require("structure/top.php"); //Include the sidebar HTML
           let size_val = parseInt(s_txt);
 
           if (validate_size(size_val) === true) {
-              setArrays(size_val);
-              // Setup modding
-              f_txt = "(" + f_txt + ")" + " % " + size_val;
-              calculate(f_txt);
+            setArrays(size_val);
+            // Setup modding
+            f_txt = "(" + f_txt + ")" + " % " + size_val;
+            calculate(f_txt);
+            //popTest();
+            draw(size_val);
           } else {
-              //reset();
-              console.log("error");
+            //reset();
+            console.log("error");
           }
         });
       });
 
+      function draw(size_val){
+        let width = document.getElementById("f_area").offsetHeight
+
+        const svgContainer = d3.select("#svghandle")
+          .append("svg")
+          .attr("width","1000")
+          .attr("height","720");
+
+        const ellipses = [
+          {"cx":  360, "cy":  300, "rx": 100, "ry": 40 * size_val},
+          {"cx":  660, "cy":  300, "rx": 100, "ry": 40 * size_val},
+        ];
+
+        const svgEllipses = svgContainer
+          .selectAll("ellipse")
+          .data(ellipses)
+          .enter()
+          .append("ellipse")
+          .style('fill', "white")
+          .style("stroke", "#000000")
+          .style("stroke-width", 2);
+
+        svgEllipses
+          .attr("cx", (d,i) => { return d.cx; })
+          .attr("cy", (d,i) => { return d.cy; })
+          .attr("rx", (d,i) => { return d.rx; })
+          .attr("ry", (d,i) => { return d.ry; });
+
+        let text = svgContainer.selectAll("text")
+                      .data(ellipses)
+                      .enter()
+                      .append("text");
+
+        let textLabels = text
+          .attr("x", function(d) { return d.cx - 2; })
+          .attr("y", function(d) { return d.cy - 150; })
+          .text( function (d) { return gDomain[0]; })
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "20px");
+
+      }
+
       function reset(){
-          document.getElementById("submit_btn").disable = true;
+        document.getElementById("submit_btn").disable = true;
+      }
+
+      function popTest(){
+        let toPrint = "";
+        for( let i = 0; i < gDomain.length; i++){
+            toPrint += gDomain[i] + " -> " + gCodomain[i] + "<br />" ;
+        }
+        document.getElementById("f_area").innerHTML = toPrint;
       }
 
       function validate_size(size_val){
@@ -52,6 +107,19 @@ require("structure/top.php"); //Include the sidebar HTML
         }
       }
 
+      function setArrays(size_val){
+        gDomain = Array.from(Array(size_val).keys());
+        gCodomain = new Array();
+      }
+
+      function calculate(f_txt){
+        // Calculate and assign mappings
+        gDomain.forEach(item => {
+            let scope = {x: item};
+            gCodomain.push( math.evaluate(f_txt, scope) );
+        })
+      }
+
       // $(document).ready(function() {
       //     $('.formfield').on('input', function() {
       //         var nFilled = $('.formfield').filter(function() {
@@ -61,21 +129,6 @@ require("structure/top.php"); //Include the sidebar HTML
       //     })
       //         .trigger('input');
       // });
-
-      function setArrays(size_val){
-          gDomain = Array.from(Array(size_val).keys());
-          gCodomain = Array(size_val);
-      }
-
-      function calculate(f_txt){
-          // Calculate and assign mappings
-          gDomain.forEach(item => {
-              let scope = {x: item};
-              gCodomain.push( math.evaluate(f_txt, scope) );
-          })
-          console.log( gDomain );
-          console.log( gCodomain );
-      }
 
     </script>
   </head>
@@ -102,7 +155,6 @@ require("structure/top.php"); //Include the sidebar HTML
         <!-- Main content -->
         <div class="content">
             <div class="container-fluid">
-
                 <div class="card card-primary card-outline">
                     <div class="card-header">
                         <h5 class="m-0">Function Mapping</h5>
@@ -124,12 +176,24 @@ require("structure/top.php"); //Include the sidebar HTML
                       <button id = "submit_btn" type="submit" class="btn btn-primary">
                         Generate Mapping
                       </button>
+                      <div class="card-footer">
+                          <div class="container-fluid">
+                            <div class="card">
+                              <div id = "f_area" class="card-body">
+                                <div id = "svghandle">
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
 
 
-            </div><!-- /.container-fluid -->
-        </div>
+          </div><!-- /.container-fluid -->
+      </div>
         <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
